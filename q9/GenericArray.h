2b5e9a14270b075448357679ccd952a4bff8ab85
kiaera <zxccxz756@gmail.com>
2026-06-19 01:37:50 +0900

    Add files via upload

---
+++ q9/GenericArray.h
@@ -0,0 +1,99 @@
+﻿/*
+GenericArray.h
+project : 다양한 자료형의 배열을 저장하고 삽입정렬하는 GenericArray template class를 구현한 헤더파일
+st_id : 22312039
+Author : Cho_ilsu
+Date of last update : May 15, 2026
+*/
+
+// GENERICARRAY_H가 선언되어 있지 않은 경우에만 포함하기
+#ifndef GENERIC_ARRAY_H
+
+// GENERICARRAY_H 매크로 정의하기
+#define GENERIC_ARRAY_H
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+// GenericArray template class 전방 선언하기
+template<typename T>
+
+// 다양한 자료형의 배열을 저장하는 template class 생성하기
+class GenericArray {
+
+    // 파일로부터 배열 데이터를 이력받기 위한 함수
+    friend std::istream& operator>>(std::istream& is, GenericArray<T>& ga) {
+        std::string title;
+        int count;
+        if (is >> title >> count) {
+            ga.num_elements = 0;
+            // 지정된 개수만큼 배열에 데이터를 읽어오기
+            for (int i = 0; i < count && i < ga.capacity; ++i) {
+                if (is >> ga.GA[i]) {
+                    ga.num_elements++;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        return is;
+    }
+
+    // 배열 데이터를 출력하기 위한 함수
+    friend std::ostream& operator<<(std::ostream& os, const GenericArray<T>& ga) {
+        os << ga.name << "(num_elements : " << ga.num_elements << ") =\n";
+        // 한줄에 10개씩 정렬하여 출력하기
+        for (int i = 0; i < ga.num_elements; ++i) {
+            os << std::setw(9) << ga.GA[i];
+            if ((i + 1) % 10 == 0 && i != ga.num_elements - 1) {
+                os << "\n";
+            }
+        }
+        return os;
+    }
+
+public:
+    // 생성자
+    GenericArray(std::string nm = "", int capacity = 20) {
+        this->name = nm;
+        this->capacity = capacity;
+        this->num_elements = 0;
+        this->GA = new T[capacity];
+    }
+
+    // 소멸자
+    ~GenericArray() {
+        if (GA != nullptr) {
+            delete[] GA;
+        }
+    }
+
+    // 삽입 정렬 실행하기
+    void insertSort() {
+        for (int i = 1; i < num_elements; ++i) {
+            T key = GA[i];
+            int j = i - 1;
+            while (j >= 0 && GA[j] > key) {
+                GA[j + 1] = GA[j];
+                j--;
+            }
+            GA[j + 1] = key;
+        }
+    }
+
+    /*
+    배열 이름, 최대 저장가능 개수, 
+    현재 저장된 원소 개수 동적배열 저장하기
+    */
+protected:
+    std::string name;
+    T* GA;
+    int num_elements;
+    int capacity; 
+};
+
+#endif
+
---
+++ q9/quiz09.cpp
@@ -0,0 +1,85 @@
+﻿/*
+main.cpp
+project : GenericArray template class를 이용하여 double, string 배열을 입력받고 삽입정렬하여 출력하는 프로그램
+st_id : 22312039
+Author : Cho_ilsu
+Date of last update : May 15, 2026
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+
+// GenericArray template class를 사용하기 위한 헤더파일
+#include "GenericArray.h"
+
+using namespace std;
+
+/*
+텍스트 파일로부터 다양한 타입의 데이터를 받아와 
+배열에 저장하고 삽입정렬하여 출력하는 함수
+*/
+int main()
+{
+    // 입력 파일 객체 선언하기
+    fstream fin_dbl, fin_str;
+
+    // 한 줄에 출력할 개수, 출력할 줄 수 저장하기
+    int per_line = 10, sample_lines = 2;
+
+    // double 데이터 파일 열기
+    fin_dbl.open("dblArray_data.txt");
+
+    // 파일 열기 실패 확인하기
+    if (fin_dbl.fail()) {
+        cout << "Error in reading dblArray_data.txt !!!" << endl;
+        exit(1);
+    }
+
+    // double형 GenericArray 객체 생성하기
+    GenericArray<double> ga_dbl("DoubleArrayData", 20);
+
+    // 파일 데이터 입력받기
+    fin_dbl >> ga_dbl;
+
+    // 파일 닫기
+    fin_dbl.close();
+
+    // 정렬 전 배열 출력하기
+    cout << "ga_dbl before sorting :\n" << ga_dbl << endl;
+
+    // 삽입정렬 수행하기
+    ga_dbl.insertSort();
+
+    // 정렬 후 배열 출력하기
+    cout << endl << "ga_dbl after sorting :\n" << ga_dbl << endl;
+
+    // string 데이터 파일 열기
+    fin_str.open("strArray_data.txt");
+
+    // 파일 열기 실패 확인하기
+    if (fin_str.fail()) {
+        cout << "Error in reading strArray_data.txt !!!" << endl;
+        exit(1);
+    }
+
+    // string형 GenericArray 객체 생성하기
+    GenericArray<string> ga_str("StringArrayData", 20); 
+
+    // 파일 데이터 입력받기
+    fin_str >> ga_str;
+
+    // 파일 닫기
+    fin_str.close();
+
+    // 정렬 전 배열 출력하기
+    cout << endl << "ga_str before sorting :\n" << ga_str << endl;
+
+    // 삽입정렬 수행하기
+    ga_str.insertSort();
+
+    // 정렬 후 배열 출력하기
+    cout << endl << "ga_str after sorting :\n" << ga_str << endl;
+
+    return 0;
+}
